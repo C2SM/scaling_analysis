bbf0f53b8fd5619133f6b40e3075fedd40c4fdb6
AnnikaLau <annika.lauber@c2sm.ethz.ch>
2021-11-19 10:00:00 +0100

    Added experiment for daint and changed some settings of the experiments

--- def_exps_plot.py
+++ def_exps_plot.py
@@ -23,16 +23,16 @@ class experiment:
 
 # Definition of each experiment properties (colors, labels,ect)
 
-euler_01 = experiment(name='atm_rte_rrtmgp_amip_test_e6_gcc02',
-                      label='Euler6, gcc.02',
-                      bestconf=35,
+euler_01 = experiment(name='atm_rte_rrtmgp_amip_test_gcc02',
+                      label='Euler7, gcc.02',
+                      bestconf=50,
                       marker='>',
                       color='#fcae91',
                       linestyle='-')
 
-euler_02 = experiment(name='atm_rte_rrtmgp_amip_test_gcc02',
-                      label='gcc.02',
-                      bestconf=35,
+euler_02 = experiment(name='atm_rte_rrtmgp_amip_test_e6_gcc02',
+                      label='Euler6, gcc.02',
+                      bestconf=200,
                       marker='x',
                       color='#fb6a4a',
                       linestyle='-')
@@ -44,16 +44,18 @@ euler_03 = experiment(name='atm_rte_rrtmgp_amip_test_gcc03',
                       color='#cb181d',
                       linestyle='-')
 
-daint_01 = experiment(name='atm_rte_rrtmgp_amip_test_gcc02',
-                      label='gcc.02',
+daint_01 = experiment(name='atm_rte_rrtmgp_amip_test_gcc01',
+                      label='gcc.01',
                       bestconf=35,
                       marker='>',
                       color='#fcae92',
                       linestyle='-')
 
-daint_02 = experiment(name='atm_rte_rrtmgp_amip_test_pgi02',
-                      label='pgi.02',
+daint_02 = experiment(name='atm_rte_rrtmgp_amip_test_gcc02',
+                      label='gcc.02',
                       bestconf=35,
                       marker='x',
                       color='#fb6a4a',
                       linestyle='-')
+
+daint_03 = experiment(name='atm_rte_rrtmgp_amip_test_pgi02', label='pgi.02', bestconf=35, marker='o', color='#cb181d',linestyle='-')
