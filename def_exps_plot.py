ed1ef87c36201765daff2c2a5e99106aadc4ba81
Annika Lauber <annika.lauber@c2sm.ethz.ch>
2021-11-19 10:21:36 +0100

    Changed experiment names from 0 to O

--- def_exps_plot.py
+++ def_exps_plot.py
@@ -23,43 +23,43 @@ class experiment:
 
 # Definition of each experiment properties (colors, labels,ect)
 
-euler_01 = experiment(name='atm_rte_rrtmgp_amip_test_gcc02',
-                      label='Euler7, gcc.02',
+euler_01 = experiment(name='atm_rte_rrtmgp_amip_test_gccO2',
+                      label='Euler7, gcc.O2',
                       bestconf=50,
                       marker='>',
                       color='#fcae91',
                       linestyle='-')
 
-euler_02 = experiment(name='atm_rte_rrtmgp_amip_test_e6_gcc02',
-                      label='Euler6, gcc.02',
+euler_02 = experiment(name='atm_rte_rrtmgp_amip_test_e6_gccO2',
+                      label='Euler6, gcc.O2',
                       bestconf=200,
                       marker='x',
                       color='#fb6a4a',
                       linestyle='-')
 
-euler_03 = experiment(name='atm_rte_rrtmgp_amip_test_gcc03',
-                      label='gcc.03',
+euler_03 = experiment(name='atm_rte_rrtmgp_amip_test_gccO3',
+                      label='gcc.O3',
                       bestconf=35,
                       marker='o',
                       color='#cb181d',
                       linestyle='-')
 
-daint_01 = experiment(name='atm_rte_rrtmgp_amip_test_gcc01',
-                      label='gcc.01',
+daint_01 = experiment(name='atm_rte_rrtmgp_amip_test_gccO1',
+                      label='gcc.O1',
                       bestconf=35,
                       marker='>',
                       color='#fcae92',
                       linestyle='-')
 
-daint_02 = experiment(name='atm_rte_rrtmgp_amip_test_gcc02',
-                      label='gcc.02',
-                      bestconf=35,
+daint_02 = experiment(name='atm_rte_rrtmgp_amip_test_gccO2',
+                      label='gcc.O2',
+                      bestconf=50,
                       marker='x',
                       color='#fb6a4a',
                       linestyle='-')
 
-daint_03 = experiment(name='atm_rte_rrtmgp_amip_test_pgi02',
-                      label='pgi.02',
+daint_03 = experiment(name='atm_rte_rrtmgp_amip_test_pgiO2',
+                      label='pgi.O2',
                       bestconf=35,
                       marker='o',
                       color='#cb181d',
